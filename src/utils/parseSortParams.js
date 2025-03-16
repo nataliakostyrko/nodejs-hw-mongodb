453bef73012df225c2d621df790ff86ee5fee6a4
nataliakostyrko <155002144+nataliakostyrko@users.noreply.github.com>
2025-03-16 19:21:23 +0100

    start

---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,34 @@
+import { SORT_ORDER } from '../constants/index.js';
+
+const parseSortBy = (sortBy) => {
+    const keysOfContacts = [
+    '_id',
+    'name',
+    'phoneNumber',
+    'email',
+    'createdAt',
+    'updateAt',
+    ];
+  if (keysOfContacts.includes(sortBy)) {
+    return sortBy;
+  }
+  return '_id';
+};
+
+const parseSortOrder = (sortOrder) => {
+  const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
+  if (isKnownOrder) return sortOrder;
+  return SORT_ORDER.ASC;
+};
+
+export const parseSortParams = (query) => {
+  const { sortBy, sortOrder } = query;
+
+  const parsedSortBy = parseSortBy(sortBy);
+  const parsedSortOrder = parseSortOrder(sortOrder);
+
+  return {
+    sortBy: parsedSortBy,
+    sortOrder: parsedSortOrder,
+  };
+};
