794963e24bcd60af47a55445547e792281e44f54
nataliakostyrko <155002144+nataliakostyrko@users.noreply.github.com>
2025-04-12 14:31:32 +0200

    commit6

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,6 +32,8 @@ export const SORT_ORDER = {
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
+export const APP_DOMAIN = 'APP_DOMAIN';
+export const BECKEND_DOMAIN = 'BECKEND_DOMAIN';
 
 
 
