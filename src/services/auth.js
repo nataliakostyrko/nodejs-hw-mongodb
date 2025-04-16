c02857724916f24e0da37f221d235a0a40c74223
nataliakostyrko <155002144+nataliakostyrko@users.noreply.github.com>
2025-04-16 19:52:41 +0200

    commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -11,6 +11,8 @@ import path from 'node:path';
 import fs from 'node:fs/promises';
 import handlebars from 'handlebars';
 import { TEMPLATES_PATH } from '../constants/index.js';
+import { getFullNameFromGoogleTokenPayload, validateCode } from '../utils/googleOAuth2.js';
+
 
 const createSession = () => {
   const accessToken = randomBytes(30).toString('base64');
@@ -153,4 +155,29 @@ export const resetPassword = async (payload) => {
     { _id: user._id },
     { password: encryptedPassword },
   );
+};
+
+export const loginOrSignupWithGoogle = async (code) => {
+  const loginTicket = await validateCode(code);
+  const payload = loginTicket.getPayload();
+  if (!payload) throw createHttpError(401);
+
+  let user = await UsersCollection.findOne({ email: payload.email });
+  if (!user) {
+    const password = await bcrypt.hash(randomBytes(10), 10);
+    user = await UsersCollection.create({
+      email: payload.email,
+      name: getFullNameFromGoogleTokenPayload(payload),
+      password,
+    });
+  }
+
+  await SessionsCollection.deleteMany({ userId: user._id });
+
+  const newSession = createSession();
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    ...newSession,
+  });
 };
