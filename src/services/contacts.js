917b203c9c24cac0fd002ab2f864a1a215bec3ed
nataliakostyrko <155002144+nataliakostyrko@users.noreply.github.com>
2025-04-24 18:01:30 +0200

    conmm

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,28 +6,27 @@ export const getAllContacts = async ({
   page = 1,
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
-  sortBy = '_id',
+  sortBy = 'name',
   filter = {},
   userId,
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find({});
+  const contactsQuery = ContactsCollection.find({userId});
 
-  if (filter.contactType) {
-    contactsQuery.where('contactType').equals(filter.contactType);
+  if (filter.type) {
+    contactsQuery.where('contactType').equals(filter.type);
   }
-  if (typeof filter.isFavourite === 'boolean') {
+  if (filter.isFavourite !== 'undefined') {
     contactsQuery.where('isFavourite').equals(filter.isFavourite);
   }
 
-  contactsQuery.where('userId').equals(userId);
+
 
   const [contactsCount, contacts] = await Promise.all([
     ContactsCollection.find({}).merge(contactsQuery).countDocuments(),
     contactsQuery
-      .find({})
       .skip(skip)
       .limit(limit)
       .sort({ [sortBy]: sortOrder })
@@ -42,8 +41,8 @@ export const getAllContacts = async ({
   };
 };
 
-export const getContactById = async ({ _id, userId }) => {
-  const contact = await ContactsCollection.findById({ _id, userId });
+export const getContactById = async (contactId, userId) => {
+  const contact = await ContactsCollection.findById({ _id:contactId, userId });
   return contact;
 };
 
@@ -52,9 +51,9 @@ export const createContact = async (payload) => {
   return contact;
 };
 
-export const updateContact = async ({ _id, userId }, payload, options = {}) => {
-  const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id, userId },
+export const updateContact = async (contactId, userId, payload, options = {}) => {
+  const updatedContact = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId, userId },
     payload,
     {
       new: true,
@@ -62,14 +61,14 @@ export const updateContact = async ({ _id, userId }, payload, options = {}) => {
       ...options,
     },
   );
-  if (!rawResult || !rawResult.value) return null;
+  if (!updatedContact || !updatedContact.value) return null;
   return {
-    contact: rawResult.value,
-    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+    contact: updatedContact.value,
+    isNew: Boolean(updatedContact?.lastErrorObject?.upserted),
   };
 };
 
-export const deleteContact = async ({ _id, userId }) => {
-  const contact = await ContactsCollection.findOneAndDelete({ _id, userId });
+export const deleteContact = async (contactId, userId) => {
+  const contact = await ContactsCollection.findOneAndDelete({ _id:contactId, userId });
   return contact;
 };
